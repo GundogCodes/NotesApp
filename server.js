85bd81f668ed0a0a93a61847b0587ad22f71e14f
Gunish <gunishsharma20@gmail.com>
2023-06-14 01:12:07 -0600

    server is lit

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+const app = require('./app')
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', ()=>{
+    console.log('connected to MongoDB')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening on ${PORT} yo!`)
+})
